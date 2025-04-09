beb9264b71420e83740f0c33b29f3ea1398019c8
Thejas Bj <145650487+ThejasBJ123@users.noreply.github.com>
2025-04-09 20:58:35 +0530

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,7 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <h6 style="text-align:center;"> Copyright @ 2024 By : RIT ISE TEAM 16 </h6>
+        
+    </div>
+</footer>
